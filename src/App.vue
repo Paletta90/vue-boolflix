def9807c231cc6fc03d05cc258a884bb2fee771f
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-22 13:50:31 +0200

    Creo ambiente di lavoro, header, main

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+
+  <div id="app">
+
+    <header>
+
+      <HeaderComp />
+
+    </header>
+
+    <main>
+
+      <MainComp />
+
+    </main>
+
+  </div>
+
+</template>
+
+<script>
+
+  // Bootstrap 5
+  import "bootstrap"
+
+  // Import componenti
+  import HeaderComp from "./components/HeaderComp.vue"
+  import MainComp from "./components/MainComp.vue"
+
+  export default {
+    name: 'App',
+
+    components: {
+      HeaderComp,
+      MainComp
+    }
+
+  }
+
+</script>
+
+<style lang="scss">
+
+  // Bootstrap 5
+  @import "bootstrap/dist/css/bootstrap.min.css";
+
+  // import file.scss
+  @import "../src/assets/style/variabili.scss";
+  @import "../src/assets/style/mixin.scss";
+
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  header{
+    height: 10vh;
+    background-color: $bg-header;
+  }
+
+  main{
+    height: 90vh;
+    background-color: $bg-main;
+  }
+</style>
