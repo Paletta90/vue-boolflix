5811f6d414722f8f228d6d3905502470b1245b61
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-26 11:41:44 +0200

    Effetto hover delle card

--- src/components/ChildMain/ChildMovieSeries/CardComp.vue
+++ src/components/ChildMain/ChildMovieSeries/CardComp.vue
@@ -1,48 +1,55 @@
 <template>
 
-    <div class="border border-secondary">
-        <!-- Copertina -->
-        <img class="w-100 h-100 d-none" :src="`https://image.tmdb.org/t/p/${imgSize}/${datiCard.poster_path}`" alt="">
+    <div class="border border-secondary flip-card">
 
-        <div class="d-block h-100 overflow-auto">
+        <div class="flip-card-inner">
 
-            <!-- Titolo del film -->
-            <span class="fw-bold">Titolo: </span>
-            <span v-if="what == 'movie'">{{datiCard.title}}</span>
-            <span v-else>{{datiCard.name}}</span>
-
-            <!-- Lingua originale con rispettiva bandiera se disponibile -->
-            <div v-if="findFlag(datiCard.original_language)">
-                <img :src="`../../../assets/flag/${String( datiCard.original_language )}.svg`" alt="" width="20px">
-            </div>
-            <div v-else><img src="../../../assets/flag/icons8-aiuto-50.png" alt="" width="20px"></div>
-
-            <!-- Titolo originale -->
-            <span class="fw-bold">Titolo orginale: </span>
-            <span v-if="what == 'movie'">{{datiCard.original_title}}</span>
-            <span v-else>{{datiCard.original_name}}</span>
-
-            <!-- Media voto -->
-            <div>
-
-                <span class="fw-bold">Voto: </span>
-
-                <!-- Stampo le stelle bianche in base alla media ottenuta -->
-                <span v-for="(elem, index) in mediaVoto" :key="'A' + index">
-                    <span><i class="fa-solid fa-star text-white"></i></span>
-                </span>
-                <span v-for="(elem, index) in (5 - mediaVoto)" :key="'B' +index">
-                    <span><i class="fa-regular fa-star text-white"></i></span>
-                </span>
-
+            <!-- Copertina -->
+            <div class="w-100 h-100 flip-card-front">
+                <img class="w-100 h-100" :src="`https://image.tmdb.org/t/p/${imgSize}/${datiCard.poster_path}`" alt="">
             </div>
 
-            <!-- Overview -->
-            <div>
-                <span class="fw-bold">Overview:</span>
-                <span>{{datiCard.overview}}</span>
-            </div>
+            <div class="h-100 overflow-auto flip-card-back">
 
+                <!-- Titolo del film -->
+                <span class="fw-bold">Titolo: </span>
+                <span v-if="what == 'movie'">{{datiCard.title}}</span>
+                <span v-else>{{datiCard.name}}</span>
+
+                <!-- Lingua originale con rispettiva bandiera se disponibile -->
+                <div v-if="findFlag(datiCard.original_language)">
+                    <img :src="`../../../assets/flag/${String( datiCard.original_language )}.svg`" alt="" width="20px">
+                </div>
+                <div v-else><img src="../../../assets/flag/notfound.png" alt="" width="20px"></div>
+
+                <!-- Titolo originale -->
+                <span class="fw-bold">Titolo orginale: </span>
+                <span v-if="what == 'movie'">{{datiCard.original_title}}</span>
+                <span v-else>{{datiCard.original_name}}</span>
+
+                <!-- Media voto -->
+                <div>
+
+                    <span class="fw-bold">Voto: </span>
+
+                    <!-- Stampo le stelle bianche in base alla media ottenuta -->
+                    <span v-for="(elem, index) in mediaVoto" :key="'A' + index">
+                        <span><i class="fa-solid fa-star text-white"></i></span>
+                    </span>
+                    <span v-for="(elem, index) in (5 - mediaVoto)" :key="'B' +index">
+                        <span><i class="fa-regular fa-star text-white"></i></span>
+                    </span>
+
+                </div>
+
+                <!-- Overview -->
+                <div>
+                    <span class="fw-bold">Overview:</span>
+                    <span>{{datiCard.overview}}</span>
+                </div>
+
+
+            </div>
 
         </div>
 
@@ -82,12 +89,42 @@
 </script>
 
 <style lang="scss" scoped>
-    img{
-        object-fit: cover;
+
+    .flip-card {
+        background-color: transparent;
+        width: 100%;
+        height: 100%;
+        perspective: 1000px;
     }
 
-        img:hover{
-            display: none;
-        }
-    
+    .flip-card-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        transition: transfosrm 0.6s;
+        transform-style: preserve-3d;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    }
+
+    .flip-card:hover .flip-card-inner {
+        transform: rotateY(180deg);
+    }
+
+    .flip-card-front,
+    .flip-card-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+    }
+
+    .flip-card-front {
+        background-color: #bbb;
+        color: black;
+    }
+
+    .flip-card-back {
+        color: white;
+        transform: rotateY(180deg);
+    }
 </style>
