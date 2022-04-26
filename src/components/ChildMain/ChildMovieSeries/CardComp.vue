075568431a668a2d7e1b2d253a9fc22c8e82970b
DavideDiPietro <davide.dipietro3@gmail.com>
2022-04-26 11:46:55 +0200

    Modifico la scrollbar

--- src/components/ChildMain/ChildMovieSeries/CardComp.vue
+++ src/components/ChildMain/ChildMovieSeries/CardComp.vue
@@ -126,4 +126,17 @@
         color: white;
         transform: rotateY(180deg);
     }
+
+    ::-webkit-scrollbar {
+        width: 2px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #888;
+    }
+
 </style>
